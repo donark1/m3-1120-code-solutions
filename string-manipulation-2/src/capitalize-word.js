0e099121300cf84ee569732a73b94e1f3dbb0585
donark1 <don_ark@sbcglobal.net>
2020-11-17 18:48:34 -0800

    String Manipulation 2 - Don Ark

---
+++ string-manipulation-2/src/capitalize-word.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-unused-vars */
+const capitalizeWord = (word) => {
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase().replace('s', 'S');
+}
---
+++ string-manipulation-2/src/truncate.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+const truncate = (length, string) => {
+  if (string.length <= length) {
+  }
+  return string.slice(0, length) + '...'
+}
