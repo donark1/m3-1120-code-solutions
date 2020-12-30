9df3289c8f3b8744d918c8a6eeca1e7940b30a00
donark1 <don_ark@sbcglobal.net>
2020-12-29 21:58:26 -0800

    SQL Join - Don Ark

---
+++ sql-join/sassy-cast.sql
@@ -0,0 +1,5 @@
+select "a"."firstName", "a"."lastName"
+  from "castMembers"
+  join "films" as "f" using ("filmId")
+  join "actors" as "a" using ("actorId")
+  where "f"."title" = 'Jersey Sassy';
---
+++ sql-join/where-are-they.sql
@@ -0,0 +1,3 @@
+select "a"."line1", "c"."name", "a"."district"
+  from "addresses" as "a"
+  join "cities" as "c" using ("cityId");
